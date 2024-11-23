3113174ff654edd46f700b22b5bb257893921488
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-11-23 13:43:33 -0500

    Update Cross-Language Bridges.c

--- Cross-Language Bridges.c
+++ Cross-Language Bridges.c
@@ -9,3 +9,18 @@ void call_python_function() {
     printf("Result: %f\n", PyFloat_AsDouble(result));
     Py_Finalize();
 }
+
+#include <Python.h>
+
+void call_python_function() {
+    Py_Initialize();
+    PyObject *module = PyImport_ImportModule("math_utils");
+    PyObject *func = PyObject_GetAttrString(module, "square_root");
+    PyObject *args = PyTuple_Pack(1, PyLong_FromLong(16));
+    PyObject *result = PyObject_CallObject(func, args);
+
+    long value = PyLong_AsLong(result);
+    printf("Square Root: %ld\n", value);
+
+    Py_Finalize();
+}

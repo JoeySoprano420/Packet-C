3953ac80f1231aa8ef21052e16dae403bf06f45f
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-11-23 13:42:59 -0500

    Update Dynamic Inline Learning.py

--- Dynamic Inline Learning.py
+++ Dynamic Inline Learning.py
@@ -10,3 +10,31 @@ class DILModel:
 
     def predict(self, input_data):
         return self.model.predict(input_data)
+
+import json
+from sklearn.linear_model import LogisticRegression
+
+class DynamicInlineLearning:
+    def __init__(self):
+        self.model = LogisticRegression()
+        self.history = []
+
+    def train(self, features, labels):
+        self.model.fit(features, labels)
+
+    def predict(self, input_features):
+        return self.model.predict([input_features])
+
+    def save_state(self, filename="model_state.json"):
+        with open(filename, "w") as f:
+            json.dump({"history": self.history}, f)
+
+    def load_state(self, filename="model_state.json"):
+        with open(filename, "r") as f:
+            data = json.load(f)
+            self.history = data["history"]
+
+# Usage example
+dil = DynamicInlineLearning()
+dil.train([[0, 1], [1, 0]], [0, 1])
+print(dil.predict([1, 1]))
